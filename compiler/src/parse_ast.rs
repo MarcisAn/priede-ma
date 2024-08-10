f53cf7dfa978b851bb6377a116da538eccfb3f57
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2024-08-10 15:56:08 +0300

    tests & multiply

--- compiler/src/parse_ast.rs
+++ compiler/src/parse_ast.rs
@@ -94,6 +94,27 @@ pub fn parse_ast(node: AstNode, compiler: &mut Compiler, block: &mut Vec<OPTCODE
         });
         compiler.stack.push_back(crate::StackValue::NUM { register: a_register });
     }
+    if title == "reiz" {
+        parse_ast(node.child(0), compiler, block);
+        parse_ast(node.child(1), compiler, block);
+        let a = compiler.stack.pop_back().unwrap();
+        let b = compiler.stack.pop_back().unwrap();
+
+        let a_register = match a {
+            crate::StackValue::NUM { register } => register,
+            _ => panic!("addition with non-number"),
+        };
+        let b_register = match b {
+            crate::StackValue::NUM { register } => register,
+            _ => panic!("addition with non-number"),
+        };
+
+        block.push(crate::OPTCODE::Add {
+            target_register: a_register,
+            value_register: b_register,
+        });
+        compiler.stack.push_back(crate::StackValue::NUM { register: a_register });
+    }
     if title == "minus" {
         parse_ast(node.child(0), compiler, block);
         parse_ast(node.child(1), compiler, block);
