8d1a8a6b4716f33ef180e599c995490e95a01c28
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2024-08-30 23:19:56 +0300

    rewrite

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,5 @@
 extern crate compiler;
 
 fn main() {
-    compiler::compile("../examples/test.pr".to_string())
+    compiler::transpile("E:/Dev/priede/examples/.sveika_pasaule.json".to_string())
 }
--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,4 +1,261 @@
-use std::{ collections::LinkedList, f32::consts::E, fs, process };
+extern crate celsium;
+use std::{ collections::LinkedList, fs, process::{ self, Output } };
+
+use celsium::{ bytecode::OPTCODE, bytecode_parser::parse_bytecode };
+
+#[derive(Debug, Clone)]
+pub enum MAOptcode {
+    LoadNumber {
+        value: String,
+        register: usize,
+    },
+    LoadString {
+        value: String,
+        register: usize,
+    },
+    Add {
+        target_register: usize,
+        value_register: usize,
+    },
+    PrintFunction {
+        register: usize,
+    },
+    JumpIfZero {
+        register_to_check: usize,
+        line_to_jump_to: usize,
+    },
+    Jump {
+        relative_target: isize,
+    },
+    EmptyLine,
+    SelectFixture {
+        id_register: usize,
+    },
+    AreVarsEqual {
+        target_reg: usize,
+        a_reg: usize,
+        b_reg: usize,
+    },
+    LargerThan {
+        target_reg: usize,
+        a_reg: usize,
+        b_reg: usize,
+    },
+    LargerEq {
+        target_reg: usize,
+        a_reg: usize,
+        b_reg: usize,
+    },
+    DefineVariable {
+        name: String,
+        value_reg: usize,
+    },
+    GetVariable {
+        name: String,
+        target_reg: usize,
+    },
+    JumpTarget
+}
+
+pub struct StackValue {
+    register: usize,
+}
+
+pub fn transpile(path: String) {
+    let bytecode_str = read_file(path);
+    let bytecode = parse_bytecode(bytecode_str);
+    let mut ma_bytecode: Vec<MAOptcode> = vec![];
+    let mut stack: LinkedList<StackValue> = LinkedList::new();
+    let mut register_counter: usize = 0;
+    for optcode in bytecode {
+        process_optcode(optcode, &mut stack, &mut ma_bytecode, &mut register_counter);
+    }
+    for macode in ma_bytecode {
+        println!("{:?}", macode);
+    }
+}
+
+pub fn process_optcode(
+    optcode: OPTCODE,
+    stack: &mut LinkedList<StackValue>,
+    output: &mut Vec<MAOptcode>,
+    register_counter: &mut usize
+) {
+    match optcode {
+        OPTCODE::LoadInt { value } => {
+            output.push(MAOptcode::LoadNumber {
+                value: value.to_string(),
+                register: *register_counter,
+            });
+            stack.push_back(StackValue { register: *register_counter });
+            *register_counter += 1;
+        }
+        OPTCODE::LoadBool { value } => {
+            output.push(MAOptcode::LoadNumber {
+                value: match value {
+                    true => "1".to_string(),
+                    false => "0".to_string(),
+                },
+                register: *register_counter,
+            });
+
+            stack.push_back(StackValue { register: *register_counter });
+            *register_counter += 1;
+        }
+        OPTCODE::LoadString { value } => {
+            output.push(MAOptcode::LoadString { value, register: *register_counter });
+            stack.push_back(StackValue { register: *register_counter });
+            *register_counter += 1;
+        }
+        OPTCODE::LoadFloat { value } => {
+            output.push(MAOptcode::LoadNumber {
+                register: *register_counter,
+                value: value.to_string(),
+            });
+            stack.push_back(StackValue { register: *register_counter });
+            *register_counter += 1;
+        }
+        OPTCODE::LoadVar { id } => {
+            output.push(MAOptcode::GetVariable {
+                name: id.to_string(),
+                target_reg: *register_counter,
+            });
+            *register_counter += 1;
+        }
+        OPTCODE::CallFunction { name } => {
+            output.push(match name.as_str() {
+                "fixture" =>
+                    MAOptcode::SelectFixture { id_register: stack.pop_back().unwrap().register },
+                _ => panic!("Unexpected name of a function"),
+            })
+        }
+        OPTCODE::CallFunctionWithBytecode { bytecode } => todo!(),
+        OPTCODE::ReturnFromFunction => todo!(),
+        OPTCODE::Add => {
+            let a = stack.pop_back().unwrap().register;
+            let b = stack.pop_back().unwrap().register;
+            output.push(MAOptcode::Add { target_register: a, value_register: b });
+            stack.push_back(StackValue { register: a });
+        }
+        OPTCODE::Subtract => {
+            let b = stack.pop_back().unwrap();
+            let a = stack.pop_back().unwrap();
+
+            let a_register = match a {
+                StackValue { register } => register,
+                _ => panic!("addition with non-number"),
+            };
+            let b_register = match b {
+                StackValue { register } => register,
+                _ => panic!("addition with non-number"),
+            };
+
+            output.push(MAOptcode::LoadString {
+                value: "\"-\"".to_string(),
+                register: *register_counter,
+            });
+            output.push(MAOptcode::Add {
+                target_register: *register_counter,
+                value_register: b_register,
+            });
+            output.push(MAOptcode::Add {
+                target_register: a_register,
+                value_register: *register_counter,
+            });
+
+            stack.push_back(StackValue { register: a_register });
+            *register_counter += 1;
+        }
+        OPTCODE::Multiply => todo!(),
+        OPTCODE::Divide => todo!(),
+        OPTCODE::Remainder => todo!(),
+        OPTCODE::LessThan => {
+            let a = stack.pop_back().unwrap().register;
+            let b = stack.pop_back().unwrap().register;
+            output.push(MAOptcode::LargerThan { target_reg: *register_counter, a_reg: a, b_reg: b });
+            stack.push_back(StackValue { register: *register_counter });
+            *register_counter += 1;
+        },
+        OPTCODE::LargerThan => {
+            let a = stack.pop_back().unwrap().register;
+            let b = stack.pop_back().unwrap().register;
+            output.push(MAOptcode::LargerThan { target_reg: *register_counter, a_reg: b, b_reg: a });
+            stack.push_back(StackValue { register: *register_counter });
+            *register_counter += 1;
+        },
+        OPTCODE::LessOrEq =>{
+            let a = stack.pop_back().unwrap().register;
+            let b = stack.pop_back().unwrap().register;
+            output.push(MAOptcode::LargerEq { target_reg: *register_counter, a_reg: b, b_reg: a });
+            stack.push_back(StackValue { register: *register_counter });
+            *register_counter += 1;
+        } ,
+        OPTCODE::LargerOrEq => {
+            let a = stack.pop_back().unwrap().register;
+            let b = stack.pop_back().unwrap().register;
+            output.push(MAOptcode::LargerEq { target_reg: *register_counter, a_reg: b, b_reg: a });
+            stack.push_back(StackValue { register: *register_counter });
+            *register_counter += 1;
+        },
+        OPTCODE::NotEq => todo!(),
+        OPTCODE::Eq => todo!(),
+        OPTCODE::Or => todo!(),
+        OPTCODE::And => todo!(),
+        OPTCODE::Xor => todo!(),
+        OPTCODE::JumpIfFalse { steps } => {
+            let register_to_check = stack.pop_back().unwrap().register;
+            output.push(MAOptcode::JumpIfZero { register_to_check, line_to_jump_to: steps });
+        }
+        OPTCODE::Jump { steps } => {
+            output.push(MAOptcode::Jump { relative_target: steps as isize });
+        },
+        OPTCODE::JumpBack { steps } => {
+            output.push(MAOptcode::Jump { relative_target: -(steps as isize) });
+        },
+        OPTCODE::DefineVar { id } => {
+            let value_reg = stack.pop_back().unwrap().register;
+            output.push(MAOptcode::DefineVariable { name: id.to_string(), value_reg });
+        },
+        OPTCODE::DefineObject { id } => todo!(),
+        OPTCODE::CreateObject { field_names } => todo!(),
+        OPTCODE::GetObjectField { field_name } => todo!(),
+        OPTCODE::DefineArray { id, init_values_count } => todo!(),
+        OPTCODE::GetFromArray { id } => todo!(),
+        OPTCODE::AssignAtArrayIndex { id } => todo!(),
+        OPTCODE::PushToArray { id } => todo!(),
+        OPTCODE::GettArrayLength { id } => todo!(),
+        OPTCODE::AssignVar { id } => {
+            let value_reg = stack.pop_back().unwrap().register;
+            output.push(MAOptcode::DefineVariable { name: id.to_string(), value_reg });
+        },
+        OPTCODE::CallSpecialFunction { function } => {
+            match function {
+                celsium::SpecialFunctions::Print { newline } =>
+                    output.push(MAOptcode::SelectFixture {
+                        id_register: stack.pop_back().unwrap().register,
+                    }),
+                celsium::SpecialFunctions::Input => todo!(),
+                celsium::SpecialFunctions::Random => todo!(),
+            }
+        }
+        OPTCODE::SimpleLoop { body_block } => todo!(),
+        OPTCODE::PushToTestingStack { duplicate_stackvalue } => {}
+    }
+
+    output.push(MAOptcode::JumpTarget);
+}
+
+pub fn read_file(path: String) -> String {
+    let file_read = fs::read_to_string(&path);
+    if file_read.is_err() {
+        println!("{}", file_read.err().unwrap());
+        println!("Neizdevās nolasīt failu {} \nPārlicinies, ka faila adrese ir pareiza!", path);
+        process::exit(1);
+    }
+    file_read.unwrap()
+}
+
+/*use std::{ collections::LinkedList, f32::consts::E, fs, process };
 mod xml_output;
 use xml_output::format_xml_file;
 use hime_redist::ast::AstNode;
@@ -245,3 +502,4 @@ mod tests {
         compile("../examples/test.pr".to_string());
     }
 }
+*/
