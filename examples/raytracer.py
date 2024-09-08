b226734587d138c7dd647540a901d6142620d7b7
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2024-09-08 14:37:56 +0300

    Revert "python stupidifying"
    This reverts commit 0b48a04a28ebbe421bda24971cb479ded6704f34.

--- examples/raytracer.py
+++ examples/raytracer.py
@@ -13,20 +13,14 @@ def sqrt(x):
             return guess
         last_guess= guess
 
-def normalize_x(x,y,z):
+def normalize(x,y,z):
+    #print(vector)
+    #print(np.linalg.norm(vector))
     xnorm = x / sqrt(x**2 + y**2 + z**2)
-    
-    return xnorm
-
-def normalize_y(x,y,z):
     ynorm = y / sqrt(x**2 + y**2 + z**2)
-    
-    return ynorm
-
-def normalize_z(x,y,z):
     znorm = z / sqrt(x**2 + y**2 + z**2)
     
-    return znorm
+    return np.array([xnorm, ynorm, znorm])
 
 def dot_product(x1,y1,z1, x2,y2,z2):
     result = x1 * x2 + y1 * y2 + z1 * z2
@@ -95,27 +89,27 @@ specular = np.array([1,1,1])
 shinines = 100
 obj_reflection = 0.5
 
-def does_intersect(orig_x, orig_y, orig_z, dir_x, dir_y, dir_z):
-    inter0 = do_spheres_intersect(obj1_center_x, obj1_center_y, obj1_center_z, obj1_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
-    inter1 = do_spheres_intersect(obj2_center_x, obj2_center_y, obj2_center_z, obj2_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
-    inter2 = do_spheres_intersect(obj3_center_x, obj3_center_y, obj3_center_z, obj3_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
-    inter3 = do_spheres_intersect(obj4_center_x, obj4_center_y, obj4_center_z, obj4_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
+def does_intersect(ray_origin, ray_direction):
+    inter0 = do_spheres_intersect(obj1_center_x, obj1_center_y, obj1_center_z, obj1_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
+    inter1 = do_spheres_intersect(obj2_center_x, obj2_center_y, obj2_center_z, obj2_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
+    inter2 = do_spheres_intersect(obj3_center_x, obj3_center_y, obj3_center_z, obj3_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
+    inter3 = do_spheres_intersect(obj4_center_x, obj4_center_y, obj4_center_z, obj4_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
     if inter0 or inter1 or inter2 or inter3:
         return True
     else:
         return False
 
-def nearest_intersected_object(orig_x, orig_y, orig_z, dir_x, dir_y, dir_z):
-    distance0 = sphere_intersect(obj1_center_x, obj1_center_y, obj1_center_z, obj1_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
-    distance1 = sphere_intersect(obj2_center_x, obj2_center_y, obj2_center_z, obj2_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
-    distance2 = sphere_intersect(obj3_center_x, obj3_center_y, obj3_center_z, obj3_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
-    distance3 = sphere_intersect(obj4_center_x, obj4_center_y, obj4_center_z, obj4_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
+def nearest_intersected_object(ray_origin, ray_direction):
+    distance0 = sphere_intersect(obj1_center_x, obj1_center_y, obj1_center_z, obj1_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
+    distance1 = sphere_intersect(obj2_center_x, obj2_center_y, obj2_center_z, obj2_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
+    distance2 = sphere_intersect(obj3_center_x, obj3_center_y, obj3_center_z, obj3_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
+    distance3 = sphere_intersect(obj4_center_x, obj4_center_y, obj4_center_z, obj4_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
     min_distance = 1000000000000.0
     
-    inter0 = do_spheres_intersect(obj1_center_x, obj1_center_y, obj1_center_z, obj1_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
-    inter1 = do_spheres_intersect(obj2_center_x, obj2_center_y, obj2_center_z, obj2_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
-    inter2 = do_spheres_intersect(obj3_center_x, obj3_center_y, obj3_center_z, obj3_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
-    inter3 = do_spheres_intersect(obj4_center_x, obj4_center_y, obj4_center_z, obj4_rad, orig_x, orig_y, orig_z, dir_x, dir_y, dir_z)
+    inter0 = do_spheres_intersect(obj1_center_x, obj1_center_y, obj1_center_z, obj1_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
+    inter1 = do_spheres_intersect(obj2_center_x, obj2_center_y, obj2_center_z, obj2_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
+    inter2 = do_spheres_intersect(obj3_center_x, obj3_center_y, obj3_center_z, obj3_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
+    inter3 = do_spheres_intersect(obj4_center_x, obj4_center_y, obj4_center_z, obj4_rad, ray_origin[0], ray_origin[1], ray_origin[2], ray_direction[0], ray_direction[1], ray_direction[2])
    
     nearest_object_index = 4
     
@@ -147,23 +141,17 @@ camera = np.array([0, 0, 1])
 ratio = float(width) / height
 screen = (-1, 1 / ratio, 1, -1 / ratio) # left, top, right, bottom
 
-
-light_pos = 5
-light_ambient = 1
-light_diffuse = 1
-light_specular = 1
+light = { 'position': np.array([5, 5, 5]), 'ambient': np.array([1, 1, 1]), 'diffuse': np.array([1, 1, 1]), 'specular': np.array([1, 1, 1]) }
 
 image = np.zeros((height, width, 3))
 for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
     for j, x in enumerate(np.linspace(screen[0], screen[2], width)):
         # screen is on origin
         pixel = np.array([x, y, 0])
-        origin_x = 0
-        origin_y = 0
-        origin_z = 1
-        tonormx= pixel[0] - origin_x
-        tonormy = pixel[1] - origin_y
-        tonormz = pixel[2] - origin_z
+        origin = camera
+        tonormx= pixel[0] - origin[0]
+        tonormy = pixel[1] - origin[1]
+        tonormz = pixel[2] - origin[2]
         
         directionx = tonormx / sqrt(tonormx**2 + tonormy**2 + tonormz**2)
         directiony = tonormy / sqrt(tonormx**2 + tonormy**2 + tonormz**2)
@@ -175,50 +163,43 @@ for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
 
         for k in range(max_depth):
             # check for intersections
-            if does_intersect(origin_x, origin_y, origin_z, directionx, directiony, directionz):
-                nearest_object, min_distance = nearest_intersected_object(origin_x, origin_y, origin_z, directionx, directiony, directionz)
+            if does_intersect(origin, np.array([directionx, directiony, directionz])):
+                nearest_object, min_distance = nearest_intersected_object(origin, np.array([directionx, directiony, directionz]))
                 if nearest_object is None:
                    break
 
-                intersection_x = origin_x + min_distance * directionx
-                intersection_y = origin_y + min_distance * directiony
-                intersection_z = origin_z + min_distance * directionz
+                intersection = origin + min_distance * np.array([directionx, directiony, directionz])
             
                 if nearest_object == 0: 
-                    to_norm_x = intersection_x - obj1_center_x
-                    to_norm_y = intersection_y - obj1_center_y
-                    to_norm_z = intersection_z - obj1_center_z
+                    to_norm_x = intersection[0] - obj1_center_x
+                    to_norm_y = intersection[1] - obj1_center_y
+                    to_norm_z = intersection[2] - obj1_center_z
                 
                 if nearest_object == 1: 
-                    to_norm_x = intersection_x - obj2_center_x
-                    to_norm_y = intersection_y - obj2_center_y
-                    to_norm_z = intersection_z - obj2_center_z
+                    to_norm_x = intersection[0] - obj2_center_x
+                    to_norm_y = intersection[1] - obj2_center_y
+                    to_norm_z = intersection[2] - obj2_center_z
                 
                 if nearest_object == 2: 
-                    to_norm_x = intersection_x - obj3_center_x
-                    to_norm_y = intersection_y - obj3_center_y
-                    to_norm_z = intersection_z - obj3_center_z
+                    to_norm_x = intersection[0] - obj3_center_x
+                    to_norm_y = intersection[1] - obj3_center_y
+                    to_norm_z = intersection[2] - obj3_center_z
                 
                 if nearest_object == 3: 
-                    to_norm_x = intersection_x - obj4_center_x
-                    to_norm_y = intersection_y - obj4_center_y
-                    to_norm_z = intersection_z - obj4_center_z
+                    to_norm_x = intersection[0] - obj4_center_x
+                    to_norm_y = intersection[1] - obj4_center_y
+                    to_norm_z = intersection[2] - obj4_center_z
             
                 normx = to_norm_x / sqrt(to_norm_x**2 + to_norm_y**2 + to_norm_z**2)
                 normy = to_norm_y / sqrt(to_norm_x**2 + to_norm_y**2 + to_norm_z**2)
                 normz = to_norm_z / sqrt(to_norm_x**2 + to_norm_y**2 + to_norm_z**2)
             
-                
-                shifted_point_x = intersection_x + 1e-5 * normx
-                shifted_point_y = intersection_y + 1e-5 * normy
-                shifted_point_z = intersection_z + 1e-5 * normz
-                
-                intersection_to_light_x = normalize_x(light_pos - shifted_point_x, light_pos - shifted_point_y,light_pos - shifted_point_z)
-                intersection_to_light_y = normalize_y(light_pos - shifted_point_x, light_pos - shifted_point_y,light_pos - shifted_point_z)
-                intersection_to_light_z = normalize_z(light_pos - shifted_point_x, light_pos - shifted_point_y,light_pos - shifted_point_z)
-                
-                _, min_distance = nearest_intersected_object(shifted_point_x, shifted_point_y, shifted_point_z, intersection_to_light_x, intersection_to_light_y, intersection_to_light_z)
-                intersection_to_light_distance = sqrt((light_pos - shifted_point_x)**2 + (light_pos - shifted_point_y)**2 + (light_pos - shifted_point_z)**2)
+                normal_to_surface = np.array([normx, normy, normz])
+                shifted_point = intersection + 1e-5 * normal_to_surface
+                intersection_to_light = normalize(light['position'][0] - shifted_point[0], light['position'][1] - shifted_point[1],light['position'][2] - shifted_point[2])
+
+                _, min_distance = nearest_intersected_object(shifted_point, intersection_to_light)
+                intersection_to_light_distance = np.linalg.norm(light['position'] - intersection)
                 is_shadowed = min_distance < intersection_to_light_distance
 
                 if is_shadowed:
@@ -228,45 +209,38 @@ for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
 
             # ambiant
                 if nearest_object == 0:
-                    illumination += obj1_ambient * light_ambient
+                    illumination += obj1_ambient * light['ambient']
                 if nearest_object == 1:
-                    illumination += obj2_ambient * light_ambient
+                    illumination += obj2_ambient * light['ambient']
                 if nearest_object == 2:
-                    illumination += obj3_ambient * light_ambient
+                    illumination += obj3_ambient * light['ambient']
                 if nearest_object == 3:
-                    illumination += obj4_ambient * light_ambient
+                    illumination += obj4_ambient * light['ambient']
 
             # diffuse
                 if nearest_object == 0:
-                    illumination += obj1_diffuse * light_diffuse * dot_product(intersection_to_light_x, intersection_to_light_y, intersection_to_light_z, normx, normy, normz)
+                    illumination += obj1_diffuse * light['diffuse'] * np.dot(intersection_to_light, normal_to_surface)
                 if nearest_object == 1:
-                    illumination += obj2_diffuse * light_diffuse * dot_product(intersection_to_light_x, intersection_to_light_y, intersection_to_light_z, normx, normy, normz)
+                    illumination += obj2_diffuse * light['diffuse'] * np.dot(intersection_to_light, normal_to_surface)
                 if nearest_object == 2:
-                    illumination += obj3_diffuse * light_diffuse * dot_product(intersection_to_light_x, intersection_to_light_y, intersection_to_light_z, normx, normy, normz)
+                    illumination += obj3_diffuse * light['diffuse'] * np.dot(intersection_to_light, normal_to_surface)
                 if nearest_object == 3:
-                    illumination += obj4_diffuse * light_diffuse * dot_product(intersection_to_light_x, intersection_to_light_y, intersection_to_light_z, normx, normy, normz)
+                    illumination += obj4_diffuse * light['diffuse'] * np.dot(intersection_to_light, normal_to_surface)
             # specular
-                intersection_to_camera_x = normalize_z(0 - intersection_x, 0 - intersection_y, 1 - intersection_z)
-                intersection_to_camera_y = normalize_y(0 - intersection_x, 0 - intersection_y, 1 - intersection_z)
-                intersection_to_camera_z = normalize_z(0 - intersection_x, 0 - intersection_y, 1 - intersection_z)
-                
-                H_x = normalize_x(intersection_to_light_x + intersection_to_camera_x, intersection_to_light_y + intersection_to_camera_y, intersection_to_light_z + intersection_to_camera_z)
-                H_y = normalize_y(intersection_to_light_x + intersection_to_camera_x, intersection_to_light_y + intersection_to_camera_y, intersection_to_light_z + intersection_to_camera_z)
-                H_z = normalize_z(intersection_to_light_x + intersection_to_camera_x, intersection_to_light_y + intersection_to_camera_y, intersection_to_light_z + intersection_to_camera_z)
-                illumination += specular * light_specular * dot_product(normx, normy, normz, H_x, H_y, H_z) ** (shinines / 4)
+                intersection_to_camera = normalize(camera[0] - intersection[0], camera[1] - intersection[1], camera[2] - intersection[2])
+                H = normalize(intersection_to_light[0] + intersection_to_camera[0], intersection_to_light[1] + intersection_to_camera[1], intersection_to_light[2] + intersection_to_light[2])
+                illumination += specular * light['specular'] * np.dot(normal_to_surface, H) ** (shinines / 4)
 
             # reflection
                 color += reflection * illumination
                 reflection *= obj_reflection
 
-                origin_x = shifted_point_x
-                origin_y = shifted_point_y
-                origin_z = shifted_point_z
+                origin = shifted_point
            
-                dot_prod = dot_product(directionx, directiony, directionz, normx, normy, normz)
-                directionx = directionx - 2 * dot_prod * normx
-                directiony = directiony - 2 * dot_prod * normy
-                directionz = directionz - 2 * dot_prod * normz
+                dot_prod = dot_product(directionx, directiony, directionz, normal_to_surface[0], normal_to_surface[1], normal_to_surface[2])
+                directionx = directionx - 2 * dot_prod * normal_to_surface[0]
+                directiony = directiony - 2 * dot_prod * normal_to_surface[1]
+                directionz = directionz - 2 * dot_prod * normal_to_surface[2]
             
 
         image[i, j] = np.clip(color, 0, 1)
